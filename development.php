ef3a4345fcd2a83effe9e8e6e661da4c1708cb65
Chris Carlevato <chris@christopherl.com>
2018-02-11 16:22:07 -0800

    Add content update

--- development.php
+++ development.php
@@ -67,11 +67,12 @@ if (!$smarty->isCached('base.tpl', $cache_id)) {
             </p>
             <p>All of the following are available for you to enjoy for free, <a href="https://en.wikipedia.org/wiki/Gratis_versus_libre" target="_blank">as in beer and speech</a>.</p>
             <ul>
-                <li><a href="https://github.com/chrislarrycarl/ChristopherL" target="_blank">ChristopherL</a> &ndash; The code that runs this very website. You're using it right now!</li>
-                <li><a href="https://github.com/chrislarrycarl/Helios-Calendar" target="_blank">Helios Calendar</a> &ndash; A once grand web calendar, now filled with nostalgia.</li>
-                <li><a href="https://github.com/chrislarrycarl/cl_database" target="_blank">cl_database</a> &ndash; A simple PHP database class.</li>
-                <li><a href="https://github.com/chrislarrycarl/cl_session" target="_blank">cl_session</a> &ndash; A friendly PHP session class.</li>
-                <li><a href="https://github.com/chrislarrycarl/dots" target="_blank">dots</a> - A collection of odds and ends to help make machine setup easier.</li>
+                <li><a href="https://github.com/christopherldotcom/ChristopherL" target="_blank">ChristopherL</a> &ndash; The code that runs this very website. You're using it right now!</li>
+                <li><a href="https://github.com/christopherldotcom/utm-dot-codes" target="_blank">utm.codes</a> &ndash; A helpful WordPress plugin.</li>
+                <li><a href="https://github.com/christopherldotcom/cl_database" target="_blank">cl_database</a> &ndash; A simple PHP database class.</li>
+                <li><a href="https://github.com/christopherldotcom/cl_session" target="_blank">cl_session</a> &ndash; A friendly PHP session class.</li>
+                <li><a href="https://github.com/christopherldotcom/Helios-Calendar" target="_blank">Helios Calendar</a> &ndash; A once grand web calendar, now filled with nostalgia.</li>
+                <li><a href="https://github.com/christopherldotcom/dots" target="_blank">dots</a> - A collection of odds and ends to help make machine setup easier.</li>
             </ul>
         </div>
     </section>
